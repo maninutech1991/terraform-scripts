ba0d48ee3deaa6e37678c9ce495fa56b459b462a
maninutech1991 <75073495+maninutech1991@users.noreply.github.com>
2025-07-11 13:39:55 +0530

    Create dcsetupv1.ps1

---
+++ dcsetupv1.ps1
@@ -0,0 +1,12 @@
+# Install the AD Domain Services role
+Install-WindowsFeature AD-Domain-Services
+
+# Promote this server to a Domain Controller with a new forest
+Install-ADDSForest `
+    -DomainName "corp.contoso.com" `
+    -SafeModeAdministratorPassword (ConvertTo-SecureString 'P@ssword1234!' -AsPlainText -Force) `
+    -InstallDNS `
+    -Force
+
+# After reboot, AD Tools will usually be installed automatically, but to ensure it:
+# You can run this part as a second script post-reboot, or add it as a scheduled task.
