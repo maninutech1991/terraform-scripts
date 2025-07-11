7933f523f48e10ad44312fb71861b00d31ff0eaa
maninutech1991 <75073495+maninutech1991@users.noreply.github.com>
2025-07-11 13:22:42 +0530

    Add files via upload

---
+++ dcsetup.ps1
@@ -0,0 +1,7 @@
+﻿Install-ADDSForest `
+    -DomainName "corp.contoso.com" `
+    -SafeModeAdministratorPassword (ConvertTo-SecureString 'P@ssword1234!' -AsPlainText -Force) `
+    -InstallDNS `
+    -Force
+
+# This will reboot automatically. Put anything else AFTER reboot as a scheduled task
