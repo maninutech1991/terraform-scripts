db2b8d4ea3b36df504820bed4b40e27a159d1c1f
maninutech1991 <75073495+maninutech1991@users.noreply.github.com>
2025-07-11 13:37:42 +0530

    Add files via upload

--- dcsetup.ps1
+++ dcsetup.ps1
@@ -1,7 +1,11 @@
-﻿Install-ADDSForest `
+﻿# Install the AD Domain Services role
+Install-WindowsFeature AD-Domain-Services
+
+# Promote this server to a Domain Controller with a new forest
+Install-ADDSForest `
     -DomainName "corp.contoso.com" `
     -SafeModeAdministratorPassword (ConvertTo-SecureString 'P@ssword1234!' -AsPlainText -Force) `
     -InstallDNS `
     -Force
 
-# This will reboot automatically. Put anything else AFTER reboot as a scheduled task
+# After reboot, AD Tools will usually be installed automatically, but to ensure it:
